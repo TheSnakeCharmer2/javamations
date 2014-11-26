1f99dcc59b9f73599e08f40d828dbe2bbb35e208
TheSnakeCharmer2 <devonrm98@gmail.com>
2014-11-26 10:44:57 -0500

    Update animation.js

--- animations/public/javascripts/animation.js
+++ animations/public/javascripts/animation.js
@@ -3,9 +3,40 @@
  */
 $(document).ready(function(){
     $("#ex1").click(function(){
-        $("div").animate({left:'250px'});
+        $("div").animate({
+            left:'250px'
+        });
     });
 });
+
+$(document).ready(function(){
+    $("#ex2").click(function(){
+        $("div").animate({
+          height:'toggle',
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#ex3").click(function(){
+        $("div").animate({
+           left:'250px',
+           height:'+=150px',
+           width:'+=150px'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#ex4").click(function(){
+        $("div").animate({
+        });
+    });
+});
+
+
+
+
 
 
 
